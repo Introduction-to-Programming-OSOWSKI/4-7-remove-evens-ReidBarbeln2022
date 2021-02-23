882c37c4c6dcc5a90b702280cd6b7b12ba36e063
ReidBarbeln2022 <814684@mystma.org>
2021-02-23 16:06:42 +0000

    remove even try 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def removeEvens(k):
+    newlist = k
+    numpoped = 0
+    for i in range (0 , len(newlist)):
+        if newlist[i - numpoped] % 2 == 0:
+            newlist.pop(i - numpoped)
+            numpoped = numpoped + 1
+    return newlist
+   
+print (removeEvens([1,2,3,4,5,6,7,8,9,10]))
