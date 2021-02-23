f0774432b2c0432de03664533a0792a090ed82a0
ReidBarbeln2022 <814684@mystma.org>
2021-02-23 16:13:41 +0000

    remove evans try2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def removeEvens(k):
     newlist = k
     numpoped = 0
     for i in range (0 , len(newlist)):
-        if newlist[i - numpoped] % 2 == 0:
+        if newlist[i - numpoped] % 2 == 0 and newlist[i - numpoped] != 0:
             newlist.pop(i - numpoped)
             numpoped = numpoped + 1
     return newlist
